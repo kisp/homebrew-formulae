679b0e9c3973c88a97d961ab814670f29949aa48
Kilian Sprotte <kilian.sprotte@gmail.com>
2017-07-28 07:04:24 +0200

    gdid 0.0.13

--- gdid.rb
+++ gdid.rb
@@ -1,9 +1,9 @@
 class Gdid < Formula
   desc "gdid. cli version"
   homepage "https://github.com/kisp/gdid"
-  url "https://github.com/kisp/gdid/archive/0.0.12.tar.gz"
-  version "0.0.12"
-  sha256 "727feda17a1ccbe6af259984fa2d1d89991b2f3c78cdcdc22579e0c862228144"
+  url "https://github.com/kisp/gdid/archive/0.0.13.tar.gz"
+  version "0.0.13"
+  sha256 "17d5593b7fb3b4c456f11941af06bf8de3d786d56eac0a1019ef5e34c5ca59f7"
 
   depends_on "sbcl" => :build
 
